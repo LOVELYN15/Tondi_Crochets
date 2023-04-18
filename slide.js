e4c79eeb981ecb621047eb6b8f06aa629b232840
LOVELYN15 <lovelynokore4@gmail.com>
2023-04-19 01:12:36 +0300

    Update slide.js

--- slide.js
+++ slide.js
@@ -1,19 +1,19 @@
-let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
+let slideIndex = 0;
+showSlides();
 
-function plusSlides(n, no) {
-    showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
+function showSlides() {
     let i;
-    let x = document.getElementsByClassName(slideId[no]);
-    if (n > x.length) {slideIndex[no] = 1}    
-    if (n < 1) {slideIndex[no] = x.length}
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";  
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+            for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
         }
-    x[slideIndex[no]-1].style.display = "block";  
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
